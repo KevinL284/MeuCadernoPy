a87e2a2ad5e3f1b5f4605657aaff791f98f1f1e4
Kevin Souza <103601328+MrKevin284@users.noreply.github.com>
2024-02-15 11:00:16 -0300

    Corrigir cálculo do IMC e adicionar impressão formatada

--- atividadis/calcimc.py
+++ atividadis/calcimc.py
@@ -1,6 +1,13 @@
 nome = 'Kevin Souza'
 altura = 1.69
 peso = 65
-imc = peso / (altura x altura)
+imc = peso / altura ** 2
 
-print(imc)
+"f-strings"
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
